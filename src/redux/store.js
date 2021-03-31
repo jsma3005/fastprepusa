bab535f4e6ed3d5ae4a24770e62a7f146829bbbb
urmat <megabmx4477@gmail.com>
2021-03-31 19:31:44 +0600

    last

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,8 @@
 import { applyMiddleware, createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import rootReducer from './reducers'
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk, logger)))
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 export default store;
